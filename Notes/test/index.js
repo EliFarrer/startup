2d39aa29c78959d08229ed0413888ad92846edaa
Eli Farrer <eli.farrer@gmail.com>
2025-02-11 16:32:41 -0700

    updated test files

--- Notes/test/index.js
+++ Notes/test/index.js
@@ -1,14 +1,26 @@
-var varCount = 20;
-let letCount = 20;
+let x = 1 + 1;
 
-console.log('Initial - var: %d, let: %d', varCount, letCount);
+console.log(x);
 
-for (var varCount = 1; varCount < 2; varCount++) {
-  for (let letCount = 1; letCount < 2; letCount++) {
-    console.log('Loop - var: %d, let: %d', varCount, letCount);
-  }
+function double(x) {
+  return x * 2;
 }
 
-const h1El = document.querySelector('h1');
-h1El.textContent = `Result - var:${varCount}, let:${letCount}`;
-console.log('Final - var: %d, let: %d', varCount, letCount);
+x = double(x);
+
+console.log(x);
+
+// var varCount = 20;
+// let letCount = 20;
+
+// console.log('Initial - var: %d, let: %d', varCount, letCount);
+
+// for (var varCount = 1; varCount < 2; varCount++) {
+//   for (let letCount = 1; letCount < 2; letCount++) {
+//     console.log('Loop - var: %d, let: %d', varCount, letCount);
+//   }
+// }
+
+// const h1El = document.querySelector('h1');
+// h1El.textContent = `Result - var:${varCount}, let:${letCount}`;
+// console.log('Final - var: %d, let: %d', varCount, letCount);
