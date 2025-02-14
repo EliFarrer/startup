fb86415424ee3a08f71b099d103ad37a41d7e494
Eli Farrer <eli.farrer@gmail.com>
2025-02-13 22:21:21 -0700

    added content for login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="container-fluid text-center">
+        <div>
+        <h2>Login to MORSE</h2>
+        <form>
+        {/* <!--login info--> */}
+        <div>
+            <span> Email: </span>
+            <input className="form-control-sm" placeholder="you@email.com" />
+        </div>
+        <div>
+            <span> Password: </span>
+            <input className="form-control-sm" type="password" placeholder="password" />
+        </div>
+        <button className="btn btn-primary" type="submit">Login</button>
+        <button className="btn btn-secondary" type="submit">Create Accont</button>
+        </form>
+        </div>
     </main>
   );
 }
