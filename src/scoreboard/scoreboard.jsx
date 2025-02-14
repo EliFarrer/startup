35e83e60576769888e20a8c46d1ec0c55238c321
Eli Farrer <eli.farrer@gmail.com>
2025-02-13 22:21:34 -0700

    added content for scoreboard.jsx

--- src/scoreboard/scoreboard.jsx
+++ src/scoreboard/scoreboard.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 export function Scoreboard() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>scoreboard displayed here</div>
+    <main className="container-fluid">
+        <div className="container-fluid" id="popup">Updates:
+            <ul className="playerUpdates">
+                <li className="playerUpdate">John got 34 letters in 1 minute.</li>
+                <li className="playerUpdate">Peter got a score of 27.</li>
+                <li className="playerUpdate">James got 13.</li>
+            </ul>
+        </div>
+
+        <h3>Logged in as <input type="text" className="form-control-lg" placeholder="Eli Farrer" /></h3>
+        <div className="container-fluid" id="popup">
+        <h1 className="scoreboardTitle"> Scoreboard</h1> 
+        <ol className="scoreboard">
+            <li className="playerScore">Johnathan: 34</li>
+            <li className="playerScore">Peter: 18</li>
+            <li className="playerScore">Eli: 1</li>
+        </ol>
+        <h3 className="loggedPlayerHighScore">Your high score: 1</h3>
+        </div>
     </main>
   );
 }
