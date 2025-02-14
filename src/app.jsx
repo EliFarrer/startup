2adc035bfe0d39826509200c07cab796160621bb
Eli Farrer <eli.farrer@gmail.com>
2025-02-13 22:10:36 -0700

    added basics for app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,45 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scoreboard } from './scoreboard/scoreboard';
 
 export default function App() {
   return (
+    <BrowserRouter>
     <body>
       <header>        
           {/* <!--navigation--> */}
-          <nav class="navbar navbar-expand-lg">
-          <div class="container-fluid">
+          <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
   
                   {/* <!-- the href in the navbar is where it takes you when you click on the image --> */}
               {/* <!-- logo --> */}
-              <a class="navbar-brand" href=".">
+              <a className="navbar-brand" href=".">
                   <img src="logo.png" alt="logo" width="30px" height="30px" />
                   _ _ _ RS .
               </a>
   
               {/* <!-- button elements --> */}
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav">
-                      <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav">
+                      <li className="nav-item">
+                          <NavLink className='nav-link' to='login'>Login</NavLink>
+                          {/* <a className="nav-link active" aria-current="page" href="index.html">Home</a> */}
                       </li>
-                      <li class="nav-item">
-                          <a class="nav-link" aria-current="page" href="play.html">Play</a>
+                      <li className="nav-item">
+                          <NavLink className='nav-link' to='play'>Play</NavLink>
+
+                          {/* <a className="nav-link" aria-current="page" href="play.html">Play</a> */}
                       </li>
                       <li>
-                          <a class="nav-link" aria-current="page" href="scoreboard.html">Scoreboard</a>
+                          <NavLink className='nav-link' to='scoreboard'>Scoreboard</NavLink>
+                          {/* <a className="nav-link" aria-current="page" href="scoreboard.html">Scoreboard</a> */}
                       </li>
                   </ul>
               </div>
@@ -38,14 +47,14 @@ export default function App() {
           </nav>
       </header>
   
-      <main class="container-fluid text-center">
+      <main className="container-fluid text-center">
         Stuff displays here!
       </main>
   
-      <footer class="container-fluid">
-          <p3>Author: Eli Farrer <a type="button" class="btn btn-link" href="https://github.com/EliFarrer/startup.git">GitHub</a></p3>
+      <footer className="container-fluid">
+          <p3>Author: Eli Farrer <a type="button" className="btn btn-link" href="https://github.com/EliFarrer/startup.git">GitHub</a></p3>
       </footer>
     </body>
-  
+    </BrowserRouter>
   )
 }
