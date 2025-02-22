1e0fbcefab48628a32e6be621bb92f791ff46e3c
Eli Farrer <eli.farrer@gmail.com>
2025-02-22 15:30:26 -0700

    added code for scores

--- src/scoreboard/scoreboard.jsx
+++ src/scoreboard/scoreboard.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
 
 export function Scoreboard() {
+  const score1 = { name:"ELI", num:'16'};
+  const score2 = { name:"KPJM", num:'26'};
+  const myscores = { score1, score2 };
+
+  localStorage.setItem('scores', JSON.stringify(myscores));
+
+  const [scores, setScores] = React.useState([]);
+
+  setTimeout(() => {
+    const text = localStorage.getItem('scores');
+    console.log(text);
+    if (text) { // if we have stuff
+      setScores(JSON.parse(text)); // converts to a javascript object
+      console.log("Scores text: " + text);
+
+    }
+  }, 5000); // only do this the first time
+
+  
+  /* this is all the code we will inject into the table */
+  const scoreEntries = [];
+  if (scores.length) { // if we have scores
+    for(const [i, score] of scores.entries()) {
+      console.log("I: " + i + " score: " + score);
+      scoreEntries.push(
+        "yeet"
+        // <li>{score.name}: {score.num}</li>
+      );
+    }
+  } else { // if we don't have any scores
+    scoreEntries.push('No scores yet... :(');
+  }
+
+
   return (
     <main className="container-fluid">
         <div className="container-fluid" id="popup">Updates:
@@ -14,10 +48,10 @@ export function Scoreboard() {
         <h3>Logged in as <input type="text" className="form-control-lg" placeholder="Eli Farrer" /></h3>
         <div className="container-fluid" id="popup">
         <h1 className="scoreboardTitle"> Scoreboard</h1> 
-        <ol className="scoreboard">
-            <li className="playerScore">Johnathan: 34</li>
+        <ol className="scoreboard">{scoreEntries}
+            {/* <li className="playerScore">Johnathan: 34</li>
             <li className="playerScore">Peter: 18</li>
-            <li className="playerScore">Eli: 1</li>
+            <li className="playerScore">Eli: 1</li> */}
         </ol>
         <h3 className="loggedPlayerHighScore">Your high score: 1</h3>
         </div>
