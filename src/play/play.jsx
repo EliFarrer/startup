cbdee10f1e81b4ef35d22d0040d501a27aa88d29
Eli Farrer <eli.farrer@gmail.com>
2025-02-24 16:37:57 -0700

    moved into different files

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { ScoreUpdates } from './scoreUpdates';
+import { MorseGame } from './morseGame';
+
 
 export function Play() {
     const [imageURL, setImageURL] = React.useState('');
@@ -8,34 +11,12 @@ export function Play() {
         setImageURL('url(/background3.png)');
     }, []);
 
+    const userName = localStorage.getItem('userName');
+
   return (
     <main className="container-fluid">
-        <div className="container-fluid" id="popup">Updates:
-        <ul className="playerUpdates">
-            <li className="playerUpdate">John got 34 letters in 1 minute.</li>
-            <li className="playerUpdate">Peter got a score of 27.</li>
-            <li className="playerUpdate">James got 13.</li>
-        </ul>
-        </div>
-
-        <div className="container-fluid text-center" id="background-image" style={{backgroundImage: imageURL}}>
-  
-
-            <div className="container-fluid" id="back">
-            <p3>Logged in as <input className="form-control-sm" placeholder="Eli Farrer" /></p3>
-            <label className="timeRemaining">Time left: <input className="form-control-sm" placeholder="30 seconds" /></label>
-            <label className="score">Score: <input className="form-control-sm" placeholder="20" /></label>
-            <h1> M </h1> 
-            <h5>Your input:<input className="form-control-sm" placeholder="_ _" /></h5>
-            <div className="tapInput">       
-                <button className="btn btn-primary" type="submit">_</button>
-                <button className="btn btn-primary" type="submit">.</button>
-            </div>
-
-                <button className="btn btn-secondary" type="submit">RESET</button>
-            </div>
-
-        </div>
+        <ScoreUpdates/>
+        <MorseGame imageURL={imageURL} userName={userName}/>
     </main>
   );
 }
