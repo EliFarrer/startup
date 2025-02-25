252359446658e8685f436a52d5fe068f1bdfa6aa
Eli Farrer <eli.farrer@gmail.com>
2025-02-24 17:10:00 -0700

    fixed formatting

--- src/play/morseGame.jsx
+++ src/play/morseGame.jsx
@@ -8,18 +8,19 @@ export function MorseGame(props) {
         <div className="container-fluid text-center" id="background-image" style={{backgroundImage: props.imageURL}}>
 
         <div className="container-fluid" id="back">
-        <p3>Logged in as {props.userName}</p3>
-        <label className="timeRemaining">Time left: <input className="form-control-sm" placeholder="30 seconds" /></label>
-        <label className="score">Score: <input className="form-control-sm" placeholder="20" /></label>
-        <h1> M </h1> 
+        <p>Logged in as <b>{props.userName}</b></p>
+        <p>Time left: 30 seconds</p>
+        <p>Score: 27</p>
+        <h3>Current Letter:</h3>
+        <h1>M</h1> 
         <h5>Your input:<input className="form-control-sm" placeholder="_ _" /></h5>
         <div className="tapInput">       
             <button className="btn btn-primary" type="submit">_</button>
             <button className="btn btn-primary" type="submit">.</button>
         </div>
-            <button className="btn btn-secondary" type="submit">RESET</button>
+        <div><button className="btn btn-success" type="submit">SUBMIT!</button></div>
+        <button className="btn btn-secondary" type="submit">RESET</button>
         </div>
-
         </div>
     )
 }
