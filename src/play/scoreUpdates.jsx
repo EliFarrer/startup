dce6cbb7ccc506708e77284c98ec483cfbb07cd6
Eli Farrer <eli.farrer@gmail.com>
2025-02-24 17:10:23 -0700

    made it so you can add different scores and things from an array

--- src/play/scoreUpdates.jsx
+++ src/play/scoreUpdates.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
 export function ScoreUpdates() {
+    const updates = [<li className="playerUpdate">John got 34 letters in 1 minute.</li>, <li className="playerUpdate">Peter got a score of 27.</li>, <li className="playerUpdate">James got 13.</li>];
+
+    // const [events, setEvent] = React.useState(updates);
+
+    function createMessageArray() {
+        let messageArray = [];
+        for (let i = 0; i < updates.length; i++) {
+            messageArray.push(
+                <li>{updates[i]}</li>
+            );
+        }
+        return messageArray;
+    }
+
     return (
         <div className="container-fluid" id="popup">Updates:
-        <ul className="playerUpdates">
-            <li className="playerUpdate">John got 34 letters in 1 minute.</li>
-            <li className="playerUpdate">Peter got a score of 27.</li>
-            <li className="playerUpdate">James got 13.</li>
-        </ul>
+        <ul className="playerUpdates">{createMessageArray()}</ul>
         </div>
     )
 }
