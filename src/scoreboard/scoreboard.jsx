f74b9794e79549aa3722fa6fc8eb99ce68be986a
Eli Farrer <eli.farrer@gmail.com>
2025-02-27 20:35:25 -0700

    change typo

--- src/scoreboard/scoreboard.jsx
+++ src/scoreboard/scoreboard.jsx
@@ -53,10 +53,10 @@ export function Scoreboard() {
 }
 
 function findUserHighScore(scores) {
-  const userName = localStorage.getItem('userName');
+  const username = localStorage.getItem('username');
   var highestScore = 0;
   for (const [i, score] of scores.entries()) {
-    if (score.name == userName) {
+    if (score.name == username) {
       if (score.num > highestScore) {
         highestScore = score.num;
       }
