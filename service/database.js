bd7112097a2daf7e23b565ffdcca67b795ecd38e
Eli Farrer <eli.farrer@gmail.com>
2025-03-21 17:35:19 -0600

    added a try connection

---
+++ service/database.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://mongo:${config.password}@${config.hostname}/?retryWrites=true&w=majority&appName=${config.host}`
+
+const client = new MongoClient(url);
+const db = client.db(morse);
+const users = client.collection('users');
+const scores = client.collection('scores');
+
+// create a test connection function and immediately call it.
+(async function tryConnection () {
+    try {
+    await db.command({ping: 1});
+    console.log(`DB connected to ${config.hostname}`);
+  } catch (ex) {
+    console.log(`Error with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})()
+
+
+
