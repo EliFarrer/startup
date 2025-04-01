450709cdf0ded8399aba8808c5bf77913c30587b
Eli Farrer <eli.farrer@gmail.com>
2025-04-01 16:55:12 -0600

    updated vite.config.js for ws

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
     proxy: {
       // any time the endpoint is /api, it will port the request over to port 3000
       '/api': 'http://localhost:3000',
-    },
+      '/ws': {
+        target:'ws://localhost:3000',
+        ws: true,
+      }
+    }
   },
 });
