53c067ed9f0c712a62046e9a37183c5026a86071
Eli Farrer <eli.farrer@gmail.com>
2025-04-02 13:14:11 -0600

    added GameEventNotifier

---
+++ src/play/gameNotifier.js
@@ -0,0 +1,51 @@
+// class EventMessage {
+//     constrcutor(message) {
+//         this.message = message;
+//     }
+// }
+
+class GameEventNotifier {
+    events = [];
+    handlers = [];
+
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol == "http:" ? "ws" : "wss";
+        // we will use /ws for all of the websocket requests
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+
+        this.socket.onmessage = async (message) => {
+            try {
+                const event = JSON.parse(await message.data.text());
+                this.receiveEvent(event);
+            } catch {}
+        }
+    }
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler) {
+        this.handlers.filter((h) => h !== handler);
+    }
+
+    broadcastScore(score) {
+        this.socket.send(score.stringify());
+    }
+    
+    // receives some kind of event (an EventMessage)
+    receiveEvent(event) {
+        this.events.push(event);
+
+        this.events.forEach((event) => {
+            this.handlers.forEach((handler) => {
+                // call the handler on the current event, this will update the list on the frontend
+                handler(event);
+            });
+        });
+    }
+}
+
+const GameNotifier = new GameEventNotifier();
+export { GameNotifier }
