fbc7680112b9e5f678e0ee44a5c65f795fa9e10e
Eli Farrer <eli.farrer@gmail.com>
2025-04-03 11:25:42 -0600

    fixed syntax errors

--- src/play/gameNotifier.js
+++ src/play/gameNotifier.js
@@ -1,16 +1,10 @@
-// class EventMessage {
-//     constrcutor(message) {
-//         this.message = message;
-//     }
-// }
-
 class GameEventNotifier {
     events = [];
     handlers = [];
 
     constructor() {
         let port = window.location.port;
-        const protocol = window.location.protocol == "http:" ? "ws" : "wss";
+        const protocol = window.location.protocol === "http:" ? "ws" : "wss";
         // we will use /ws for all of the websocket requests
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
 
@@ -19,7 +13,7 @@ class GameEventNotifier {
                 const event = JSON.parse(await message.data.text());
                 this.receiveEvent(event);
             } catch {}
-        }
+        };
     }
 
     addHandler(handler) {
